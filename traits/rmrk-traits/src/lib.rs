8b64b1be4781fcd968fdf0bfddf0242f8dae5fe3
iTranscend <llordbanks500@gmail.com>
2022-11-03 12:01:57 +0100

    feat: add rmrk pallets' traits dependency

---
+++ traits/rmrk-traits/src/lib.rs
@@ -0,0 +1,39 @@
+// Copyright (C) 2021-2022 RMRK
+// This file is part of rmrk-substrate.
+// License: Apache 2.0 modified by RMRK, see LICENSE.md
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub mod base;
+pub mod collection;
+pub mod nft;
+pub mod part;
+pub mod phantom_type;
+pub mod priority;
+pub mod property;
+pub mod resource;
+mod serialize;
+pub mod theme;
+
+pub use base::{Base, BaseInfo};
+pub use part::{EquippableList, FixedPart, PartType, SlotPart};
+pub use theme::{Theme, ThemeProperty};
+// pub use part::{PartInfo};
+pub use collection::{Collection, CollectionInfo};
+pub use nft::{AccountIdOrCollectionNftTuple, Nft, NftChild, NftInfo, RoyaltyInfo};
+pub use priority::Priority;
+pub use property::{Property, PropertyInfo};
+pub use resource::{
+	BasicResource, ComposableResource, Resource, ResourceInfo, ResourceInfoMin, ResourceTypes,
+	SlotResource,
+};
+pub mod primitives {
+	pub type CollectionId = u32;
+	pub type ResourceId = u32;
+	pub type NftId = u32;
+	pub type BaseId = u32;
+	pub type SlotId = u32;
+	pub type PartId = u32;
+	pub type ZIndex = u32;
+}
+pub use phantom_type::PhantomType;
---
+++ traits/rmrk-traits/src/nft.rs
@@ -0,0 +1,124 @@
+// Copyright (C) 2021-2022 RMRK
+// This file is part of rmrk-substrate.
+// License: Apache 2.0 modified by RMRK, see LICENSE.md
+#![allow(clippy::too_many_arguments)]
+
+use codec::{Decode, Encode};
+use scale_info::TypeInfo;
+use sp_runtime::DispatchError;
+use sp_std::cmp::Eq;
+
+use frame_support::pallet_prelude::*;
+use sp_runtime::Permill;
+
+use crate::{primitives::*, serialize};
+use sp_std::result::Result;
+
+#[cfg(feature = "std")]
+use serde::Serialize;
+
+#[derive(Encode, Decode, Eq, PartialEq, Copy, Clone, Debug, TypeInfo, MaxEncodedLen)]
+#[cfg_attr(feature = "std", derive(Serialize))]
+pub enum AccountIdOrCollectionNftTuple<AccountId> {
+	AccountId(AccountId),
+	CollectionAndNftTuple(CollectionId, NftId),
+}
+
+/// Royalty information (recipient and amount)
+#[cfg_attr(feature = "std", derive(PartialEq, Eq, Serialize))]
+#[derive(Encode, Decode, Debug, TypeInfo, MaxEncodedLen)]
+pub struct RoyaltyInfo<AccountId, RoyaltyAmount> {
+	/// Recipient (AccountId) of the royalty
+	pub recipient: AccountId,
+	/// Amount (Permill) of the royalty
+	pub amount: RoyaltyAmount,
+}
+
+/// Nft info.
+#[cfg_attr(feature = "std", derive(PartialEq, Eq, Serialize))]
+#[derive(Encode, Decode, Debug, TypeInfo, MaxEncodedLen)]
+#[cfg_attr(
+	feature = "std",
+	serde(bound = r#"
+			AccountId: Serialize,
+			RoyaltyAmount: Serialize,
+			BoundedString: AsRef<[u8]>
+		"#)
+)]
+pub struct NftInfo<AccountId, RoyaltyAmount, BoundedString> {
+	/// The owner of the NFT, can be either an Account or a tuple (CollectionId, NftId)
+	pub owner: AccountIdOrCollectionNftTuple<AccountId>,
+	/// Royalty (optional)
+	pub royalty: Option<RoyaltyInfo<AccountId, RoyaltyAmount>>,
+
+	/// Arbitrary data about an instance, e.g. IPFS hash
+	#[cfg_attr(feature = "std", serde(with = "serialize::vec"))]
+	pub metadata: BoundedString,
+
+	/// Equipped state
+	pub equipped: bool,
+	/// Pending state (if sent to NFT)
+	pub pending: bool,
+	/// transferability ( non-transferable is "souldbound" )
+	pub transferable: bool,
+}
+
+#[cfg_attr(feature = "std", derive(PartialEq, Eq, Serialize))]
+#[derive(Encode, Decode, TypeInfo, MaxEncodedLen)]
+pub struct NftChild {
+	pub collection_id: CollectionId,
+	pub nft_id: NftId,
+}
+
+/// Abstraction over a Nft system.
+#[allow(clippy::upper_case_acronyms)]
+pub trait Nft<AccountId, BoundedString, BoundedResourceVec> {
+	type MaxRecursions: Get<u32>;
+
+	fn nft_mint(
+		sender: AccountId,
+		owner: AccountId,
+		nft_id: NftId,
+		collection_id: CollectionId,
+		royalty_recipient: Option<AccountId>,
+		royalty_amount: Option<Permill>,
+		metadata: BoundedString,
+		transferable: bool,
+		resources: Option<BoundedResourceVec>,
+	) -> Result<(CollectionId, NftId), DispatchError>;
+	fn nft_mint_directly_to_nft(
+		sender: AccountId,
+		owner: (CollectionId, NftId),
+		nft_id: NftId,
+		collection_id: CollectionId,
+		royalty_recipient: Option<AccountId>,
+		royalty_amount: Option<Permill>,
+		metadata: BoundedString,
+		transferable: bool,
+		resources: Option<BoundedResourceVec>,
+	) -> Result<(CollectionId, NftId), DispatchError>;
+	fn nft_burn(
+		owner: AccountId,
+		collection_id: CollectionId,
+		nft_id: NftId,
+		max_burns: u32,
+	) -> Result<(CollectionId, NftId), DispatchError>;
+	fn nft_send(
+		sender: AccountId,
+		collection_id: CollectionId,
+		nft_id: NftId,
+		new_owner: AccountIdOrCollectionNftTuple<AccountId>,
+	) -> Result<(AccountId, bool), DispatchError>;
+	fn nft_accept(
+		sender: AccountId,
+		collection_id: CollectionId,
+		nft_id: NftId,
+		new_owner: AccountIdOrCollectionNftTuple<AccountId>,
+	) -> Result<(AccountId, CollectionId, NftId), DispatchError>;
+	fn nft_reject(
+		sender: AccountId,
+		collection_id: CollectionId,
+		nft_id: NftId,
+		max_recursions: u32,
+	) -> Result<(AccountId, CollectionId, NftId), DispatchError>;
+}
